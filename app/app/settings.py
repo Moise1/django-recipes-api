0a24427c81c3c2a5969a7c2b662321f1aa2dd6fd
Moise1 <rwibutso.mo@gmail.com>
2023-02-20 16:36:50 +0200

    add tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
         },
     },
 ]
- 
+
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
