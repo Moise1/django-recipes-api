27c57919e36eee12aed8ab9dfa9bebc89531ef68
Moise1 <rwibutso.mo@gmail.com>
2023-02-22 10:31:07 +0200

    fix linting issues

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,7 +1,3 @@
-from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2OpError
-from django.core.management import call_command
-from django.db.utils import OperationalError
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.test import Client
@@ -9,37 +5,32 @@ from django.urls import reverse
 
 
 class AdminSiteTests(TestCase):
-
     def setUp(self):
-
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'test123'
+            email="admin@example.com", password="test123"
         )
 
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name='Test User'
+            email="user@example.com", password="testpass123", name="Test User"
         )
 
     def test_users_list(self):
-        url = reverse('admin:core_user_changelist')
+        url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
     def test_edit_user(self):
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse("admin:core_user_change", args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-        url = reverse('admin:core_user_add')
+        url = reverse("admin:core_user_add")
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
